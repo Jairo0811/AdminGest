957d1ae73263ed12129a2fff6766e824ea22dcdb
Jairo Matías <jairo_1829@hotmail.com>
2026-08-02 01:48:24 -0400

    refactor: reutilizar contrato de actividades

--- apps/api/src/modules/activities/dto/update-activity.dto.ts
+++ apps/api/src/modules/activities/dto/update-activity.dto.ts
@@ -1,10 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
-import { IsIn, IsOptional } from 'class-validator';
 import { CreateActivityDto } from './create-activity.dto';
 
-export class UpdateActivityDto extends PartialType(CreateActivityDto) {
-  @IsOptional()
-  @IsIn(['PENDING', 'COMPLETED', 'CANCELLED'])
-  status?: string;
-}
-
+export class UpdateActivityDto extends PartialType(CreateActivityDto) {}
