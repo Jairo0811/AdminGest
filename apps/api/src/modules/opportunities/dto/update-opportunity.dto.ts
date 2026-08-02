03bb3cd481fbccb8cab6f941439db64bce54ff54
Jairo Matías <jairo_1829@hotmail.com>
2026-08-02 01:47:49 -0400

    refactor: reutilizar contrato de oportunidades

--- apps/api/src/modules/opportunities/dto/update-opportunity.dto.ts
+++ apps/api/src/modules/opportunities/dto/update-opportunity.dto.ts
@@ -1,14 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
-import { IsIn, IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 import { CreateOpportunityDto } from './create-opportunity.dto';
 
 export class UpdateOpportunityDto extends PartialType(CreateOpportunityDto) {
   @IsOptional()
-  @IsIn(['OPEN', 'WON', 'LOST'])
-  status?: string;
-
-  @IsOptional()
   @IsString()
   lostReason?: string;
 }
-
